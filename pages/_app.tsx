5ccda71607478c77e9eea4fc7bba9fd483e88296
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-24 14:16:30 +0100

    removed stuff from template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ChakraProvider>
-  );
+  ) // prettier-ignore
 }
 export default MyApp;
