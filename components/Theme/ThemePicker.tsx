62362d1a43ca99a3f33342ce069b865986f1cf36
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-24 14:50:17 +0100

    experimenting

--- components/Theme/ThemePicker.tsx
+++ components/Theme/ThemePicker.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconButton, useColorMode, Box } from "@chakra-ui/react";
 import { FaSun, FaMoon } from "react-icons/fa";
+import DarkModeToggle from "react-dark-mode-toggle";
 
 const ThemePicker: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box textAlign="right" py={5} mr={{ base: 5, lg: 10 }}>
+      <DarkModeToggle
+        onChange={toggleColorMode}
+        checked={colorMode === "dark" ? true : false}
+        size={80}
+      />
       <IconButton
         aria-label="hi"
         colorScheme={colorMode}
