ad596c448d8fd29a840f08e517fd88c7866db443
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-24 15:10:47 +0100

    updated ttheme

--- components/Theme/theme.ts
+++ components/Theme/theme.ts
@@ -1,10 +1,19 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const styles = {
+  global: (props: any) => ({
+    body: {
+      bg: mode("gray.50", "gray.900")(props),
+    },
+  }),
+};
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
-  useSystemColorMode: false,
+  useSystemColorMode: true,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, styles });
 
 export default theme;
