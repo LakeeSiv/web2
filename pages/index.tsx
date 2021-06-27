9a9b91f1088fe9b5d78524a5df87d5da58b11665
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-27 14:27:02 +0100

    added delay

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 import Layout from "../components/Layout";
 
 export default function Home() {
+  const bgColor = useColorModeValue("gray.100", "gray.900");
+
   return (
     <div>
       <Layout title="Home" active_page="Home">
-        <p>Hi</p>
+        <Box
+          bgColor={bgColor}
+          transitionProperty="colors"
+          transitionDuration="slow"
+          transitionTimingFunction="ease-out"
+        >
+          Test
+        </Box>
       </Layout>
-      <p>Hi s</p>
     </div>
   );
 }
