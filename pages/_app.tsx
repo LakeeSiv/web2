c69edc0e3b6c0938aef0ba6c7b857f50fcbf4693
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-27 13:43:24 +0100

    colormode script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "../components/Theme/theme";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Component {...pageProps} />
     </ChakraProvider>
   ) // prettier-ignore
