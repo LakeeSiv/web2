52892c9bef338b627fdfe7b9618fe97d438a7dce
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-06-27 16:57:20 +0100

    initialized server

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+config();
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+const PORT: number | string = process.env.PORT || 5000;
+
+app.get("/api", async (req: Request, res: Response) => {
+  res.json(data);
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: "page does not exist" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
